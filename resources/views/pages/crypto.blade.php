369e1c3722e4fdae90f9e6d4580fa45cd85b6a3c
mcpat12 <57865338+mcpat12@users.noreply.github.com>
2020-02-15 12:48:28 -0500

    Update crypto.blade.php

--- resources/views/pages/crypto.blade.php
+++ resources/views/pages/crypto.blade.php
@@ -61,7 +61,7 @@
       <div class="jumbotron">
         <div class="container">
           <h1 class="display-3">Category - Cryptography</h1>
-          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+          <p></p>
           <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
         </div>
       </div>
@@ -71,17 +71,17 @@
         <div class="row">
           <div class="col-md-4">
             <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p>To solve this challenge you must decipher the message to get the flag. Hopefully your mission will be more sucessful than Apollo 13.</p>
             <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
           </div>
           <div class="col-md-4">
             <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p>To solve this challenge you must decipher the message more than once. Just remember the key of this challenge is to get the flag.</p>
             <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
           </div>
           <div class="col-md-4">
             <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <p>To solve this challenge you must decipher the message multiple times. Just keep in mind that the flag is what you are looking for.</p>
             <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
           </div>
         </div>
